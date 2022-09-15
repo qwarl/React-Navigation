72b0969d6f7bb8444f54b8257c61a6eae4f3ae09
Your Name <quannngoc98@gmail.com>
2022-09-15 16:28:04 +0700

    feat: insert and add button at the same position

--- screens/AddFCL.js
+++ screens/AddFCL.js
@@ -7,13 +7,13 @@ import {
 	Platform,
 	Dimensions,
 	Alert,
+	Button
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import FormDropDownMonth from "../components/FormDropDownMonth";
 import FormDropdownContinent from "../components/FormDropdownContinent";
 import FormInput from "../components/FormInput";
 import FormDropdownStyleFCL from "../components/FormDropdownStyleFCL";
-import DateTimePicker from "@react-native-community/datetimepicker";
 import color from "./../contains/color";
 import axios from "axios";
 import SelectList from "react-native-dropdown-select-list";
@@ -22,10 +22,45 @@ import { Continent } from "../contains/continent";
 import { Container } from "../contains/container";
 import FormSubmitButton from "./../components/FormSubmitButton";
 import DropDownPicker from 'react-native-dropdown-picker';
-
+// import DateTimePicker from "@react-native-community/datetimepicker";
+import DateTimePickerModal from "react-native-modal-datetime-picker"
+import DateTimePicker from '@react-native-community/datetimepicker';
 const { width, height } = Dimensions.get("window");
 
 const Add = ({ navigation, route }) => {
+
+	// const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+	// const [dateTime, setDateTime] = useState('');
+	// const showDatePicker = () => {
+	// 	setDatePickerVisibility(true);
+	// };
+
+	// const hideDatePicker = () => {
+	// 	setDatePickerVisibility(false);
+	// };
+
+	// const handleConfirm = (date) => {
+	// 	console.warn("A date has been picked: ", date);
+	// 	hideDatePicker();
+	// };
+
+	const [showCalendar, setShowCalendar] = useState(false);
+
+	const [date1, setDate1] = useState(new Date(2000, 0, 1));
+	const [dob, setDob] = useState();
+
+
+	const handleOnChangeDatePicker = (event, selectedDate) => {
+		const currentDate = selectedDate || date;
+		setShowCalendar(false);
+		setDate(currentDate);
+		setDob(currentDate);
+	}
+
+	const showDatePicker = () => {
+		setShowCalendar(true);
+	}
+
 	let { container,
 		continent,
 		month,
@@ -40,7 +75,7 @@ const Add = ({ navigation, route }) => {
 		freeTime,
 		lines,
 		notes } = route?.params || {};
-	// console.log('item', route.params.container)
+	// console.log('item', route.params)
 
 	const handleOnChangeText = (value, fieldName) => {
 		setFclInfo({ ...fclInfo, [fieldName]: value });
@@ -129,19 +164,9 @@ const Add = ({ navigation, route }) => {
 						<SelectList
 							setSelected={(value) => setFclInfo({ ...fclInfo, month: value })}
 							data={Month}
-						// defaultOption={{ key: month, value: month }}
-						defaultOption={{ key:month, value: month}}
+							// defaultOption={{ key: month, value: month }}
+							defaultOption={{ key: month, value: month }}
 						/>
-
-						{/* <DropDownPicker
-							open={openMonth}
-							value={valueMonth}
-							items={itemsMonth}
-							setOpen={setOpenMonth}
-							setValue={setValueMonth}
-							setItems={setItemsMonth}
-							style={{}}
-						/> */}
 
 					</View>
 					<View style={styles.dropMenu}>
@@ -150,14 +175,7 @@ const Add = ({ navigation, route }) => {
 							setSelected={(value) => setFclInfo({ ...fclInfo, continent: value })}
 							data={Continent}
 						/>
-						{/* <DropDownPicker
-							open={openContinent}
-							value={valueContinent}
-							items={itemsContinent}
-							setOpen={setOpenContinent}
-							setValue={setValueContinent}
-							setItems={setItemsContinent}
-						/> */}
+
 					</View>
 					<View style={styles.dropMenu}>
 						<Text style={styles.label}>Chọn Loại Container</Text>
@@ -165,20 +183,9 @@ const Add = ({ navigation, route }) => {
 							setSelected={(value) => setFclInfo({ ...fclInfo, container: value })}
 							data={Container}
 						/>
-						{/* <DropDownPicker
-							open={openContainer}
-							value={valueContainer}
-							items={itemsContainer}
-							setOpen={setOpenContainer}
-							setValue={setValueContainer}
-							setItems={setItemsContainer}
-						/> */}
+
 					</View>
 				</View>
-				{/* </ScrollView> */}
-				{/* <FormDropdownContinent label="Chọn Châu" />
-				<FormDropdownStyleFCL label="Chọn Loại Container" /> */}
-				{/* <ScrollView style={{ marginTop: 55 }}> */}
 
 				<FormInput
 					label="POL"
@@ -241,6 +248,45 @@ const Add = ({ navigation, route }) => {
 					onChangeText={(value) => handleOnChangeText(value, "valid")}
 					onPress={addDate}
 				/>
+				<View>
+					{/* <View >
+						<TouchableOpacity style={{ flex: 1, flexDirection: 'row' }} onPress={showDatePicker}>
+							<Text style={{
+								backgroundColor: "#fff",
+								borderRadius: 2,
+								// borderWidth: 1,
+								// borderColor: "#00355A",
+								paddingLeft: 12,
+								corlor: "blue",
+								paddingRight: 12,
+								paddingTop: 5,
+								paddingBottom: 5, flex: 1
+							}}>
+								{dateTime}</Text> */}
+					{/* <Image source={images.DATE_ICON} style={styles.iconStyle} /> */}
+					{/* </TouchableOpacity>
+					</View> */}
+					{/* <DateTimePickerModal
+						isVisible={isDatePickerVisible}
+						mode="date"
+						onConfirm={handleConfirm}
+						onCancel={hideDatePicker}
+					/> */}
+					<View>
+						{/* {showCalendar && ( */}
+						<DateTimePicker
+							testID="dateTimePicker"
+							// value={date1}
+							value={new Date(2000, 0, 1)}
+							mode='date'
+							is24Hour={true}
+							display="default"
+							onChange={handleOnChangeDatePicker}
+
+						/>
+						{/* )} */}
+					</View>
+				</View>
 
 				<FormInput
 					placeholder="NOTES"
@@ -248,20 +294,43 @@ const Add = ({ navigation, route }) => {
 					onChangeText={(value) => handleOnChangeText(value, "notes")}
 					value={fclInfo.notes}
 				/>
-				<View
-					style={{
-						flex: 1,
-						marginVertical: 30,
-						marginHorizontal: 80,
-						justifyContent: "center",
-					}}
-				>
-					<TouchableOpacity style={[styles.buttonInsert]} onPress={submitForm}>
-						<Text style={{ fontSize: 18, color: "#fff" }}>Insert</Text>
-					</TouchableOpacity>
-				</View>
-			</ScrollView>
-		</View>
+				{
+					!route.params &&
+					(
+						<View
+							style={{
+								flex: 1,
+								marginVertical: 30,
+								marginHorizontal: 80,
+								justifyContent: "center",
+							}}
+						>
+							<TouchableOpacity style={styles.buttonInsert} onPress={submitForm}>
+								<Text style={{ fontSize: 18, color: "#fff" }}>Insert</Text>
+							</TouchableOpacity>
+						</View>
+					)
+				}
+				{
+					// insertOrUpdate==1&&
+					route.params &&
+					(
+						<View
+							style={{
+								flex: 1,
+								marginVertical: 30,
+								marginHorizontal: 80,
+								justifyContent: "center",
+							}}
+						>
+							<TouchableOpacity style={styles.buttonUpdate} onPress={submitForm}>
+								<Text style={{ fontSize: 18, color: "#fff" }}>Update</Text>
+							</TouchableOpacity>
+						</View>
+					)
+				}
+			</ScrollView >
+		</View >
 	);
 };
 
@@ -303,6 +372,20 @@ const styles = StyleSheet.create({
 		borderRadius: 8,
 		justifyContent: "center",
 		alignItems: "center",
+		// opacity: 0.5,
+		// zIndex:-1
+	},
+	buttonUpdate: {
+		height: 45,
+		backgroundColor: "rgba(27,27,51,1)",
+		// backgroundColor: "green",
+		borderRadius: 8,
+		justifyContent: "center",
+		alignItems: "center",
+		// opacity: 0.5,
+		// marginTop: -165,
+		zIndex: -1
+
 	},
 });
 
